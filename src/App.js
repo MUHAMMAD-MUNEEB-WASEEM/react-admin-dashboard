148ed694e4cc73400cce498c7bd440221fa4d732
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-27 07:41:45 +0500

    completed sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar';
 import TopBar from  './components/TopBar/TopBar'
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
+
         <TopBar/>
+
+        <div className="container">
+          <Sidebar/> 
+          <div className="other">Other Pages</div>
+        </div>
+
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import './Sidebar.css'
+
+//material ui
+import {
+    LineStyle,
+    Timeline,
+    TrendingUp,
+    PermIdentity,
+    Storefront,
+    AttachMoney,
+    BarChart,
+    MailOutline,
+    DynamicFeed,
+    ChatBubbleOutline,
+    WorkOutline,
+    Report,
+  } from "@material-ui/icons";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+
+            <div className="sidebar__wrapper">
+
+                {/*First Menu*/}
+
+                <div className="sidebar__menu">
+                   
+                    <h3 className="sidebar__title">Dashboard</h3>
+                   
+                    <ul className="sidebar__list">
+                      
+                        <li className="sidebarList__item active">
+                            <LineStyle className="sidebar__icon" />
+                            Home
+                        </li>
+
+
+                        <li className="sidebarList__item">
+                            <Timeline className="sidebar__icon" />
+                            Analytics
+                        </li>
+
+                        <li className="sidebarList__item">
+                           <TrendingUp className="sidebar__icon" />
+                            Sales
+                        </li>
+
+                    </ul>
+                </div>
+
+                {/*Second Menu*/}
+
+                <div className="sidebar__menu">
+                   
+                <h3 className="sidebar__title">Quick Menu</h3>
+                   
+                    <ul className="sidebar__list">
+                      
+                        <li className="sidebarList__item active">
+                            <PermIdentity className="sidebar__icon" />
+                            Users
+                        </li>
+
+
+                        <li className="sidebarList__item">
+                            <Storefront className="sidebar__icon" />
+                            Products
+                        </li>
+
+                        <li className="sidebarList__item">
+                           <AttachMoney className="sidebar__icon" />
+                            Transactions
+                        </li>
+
+                    </ul>
+                </div>
+
+                {/*Third Menu*/}
+                
+                <div className="sidebar__menu">
+                   
+                    <h3 className="sidebar__title">Notifications</h3>
+                   
+                    <ul className="sidebar__list">
+                      
+                        <li className="sidebarList__item active">
+                            <MailOutline className="sidebar__icon" />
+                            Mail
+                        </li>
+
+
+                        <li className="sidebarList__item">
+                            <DynamicFeed className="sidebar__icon" />
+                            Feedback
+                        </li>
+
+                        <li className="sidebarList__item">
+                           <ChatBubbleOutline className="sidebar__icon" />
+                            Messages
+                        </li>
+
+                    </ul>
+                </div>
+
+                {/*Fourth Menu*/}
+
+                <div className="sidebar__menu">
+                   
+                    <h3 className="sidebar__title">Staff</h3>
+                   
+                    <ul className="sidebar__list">
+                      
+                        <li className="sidebarList__item active">
+                            <WorkOutline className="sidebar__icon" />
+                            Manage
+                        </li>
+
+
+                        <li className="sidebarList__item">
+                            <Timeline className="sidebar__icon" />
+                            Analytics
+                        </li>
+
+                        <li className="sidebarList__item">
+                           <Report className="sidebar__icon" />
+                            Reports
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar 
