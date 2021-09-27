3eb204ae7e451643f6aedebd007bb0f73260b3b1
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-27 11:07:00 +0500

    done with widgets small component

---
+++ src/components/WidgetsLarge/WidgetsLarge.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WidgetsLarge.css'
+
+function WidgetsLarge() {
+    return (
+        <div className="widgetsLarge">
+            Large
+        </div>
+    )
+}
+
+export default WidgetsLarge
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Chart from '../components/Chart/Chart'
 import FeaturedInfo from '../components/FeaturedInfo/FeaturedInfo'
+import WidgetsLarge from '../components/WidgetsLarge/WidgetsLarge'
+import WidgetsSmall from '../components/WidgetsSmall/WidgetsSmall'
 import { userData } from '../DummyData'
 import './Home.css'
 
@@ -11,6 +13,10 @@ function Home() {
         <div className="home">
             <FeaturedInfo/>
             <Chart data={userData} title="User Analytics" grid dataKey="Active Users"/>
+            <div className="home__widgets">
+                <WidgetsSmall/>
+                <WidgetsLarge/>
+            </div>
         </div>
     )
 }
