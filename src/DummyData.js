7e35bd8b2388c66c35a1fc1c4e50f6140353021a
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-27 08:43:26 +0500

    created chart using recharts

---
+++ src/DummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active Users": "1000"
+    },
+    {
+        name: 'Feb',
+        "Active Users": "1500"
+    },
+    {
+        name: 'March',
+        "Active Users": "2000"
+    },
+    {
+        name: 'Apr',
+        "Active Users": "1800"
+    },
+    {
+        name: 'May',
+        "Active Users": "2200"
+    },
+    {
+        name: 'Jun',
+        "Active Users": "2800"
+    },
+    {
+        name: 'Jul',
+        "Active Users": "3000"
+    },
+    {
+        name: 'Aug',
+        "Active Users": "3200"
+    },
+    {
+        name: 'Sep',
+        "Active Users": "3000"
+    },
+    {
+        name: 'Oct',
+        "Active Users": "2700"
+    },
+    {
+        name: 'Nov',
+        "Active Users": "2600"
+    },
+    {
+        name: 'Dec',
+        "Active Users": "2500"
+    },
+    
+  ];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Chart from '../components/Chart/Chart'
 import FeaturedInfo from '../components/FeaturedInfo/FeaturedInfo'
+import { userData } from '../DummyData'
 import './Home.css'
+
 
 
 function Home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Active Users"/>
         </div>
     )
 }
